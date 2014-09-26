3f9ef8f70dcdbc765437a46c7f664b1ce63fd271
linlinOnly <616684889@qq.com>
2014-09-27 07:55:30 +0800

    update

---
+++ Source/Community/Community/Services/SverviceBase.m
@@ -0,0 +1,13 @@
+//
+//  SverviceBase.m
+//  Community
+//
+//  Created by PYL on 14-9-27.
+//  Copyright (c) 2014年 QiMENG_LYS. All rights reserved.
+//
+
+#import "SverviceBase.h"
+
+@implementation SverviceBase
+
+@end
